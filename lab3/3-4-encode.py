56a71688dbdd2ea29739409e7f169425152060b4
nikita <nikitan-5@mail.ru>
2025-06-20 09:23:05 +0300

    end.

--- lab3/3-4-encode.py
+++ lab3/3-4-encode.py
@@ -6,13 +6,17 @@ import matplotlib.pyplot as plt
 import scipy.io.wavfile as wav
 
 Fs, x = wav.read("lab3/data files/test.wav")
+# Band filter (N = 101)
 b= remez(101, [0, 200, 300, 3000, 3100, Fs/2], [0, 1, 0], fs=Fs)
 x = lfilter(b, 1.0, x)
 wa = 3500 * 2 * np.pi
 t = np.arange(0, x.shape[0]) / Fs
+# Hilbert (N = 101)
 b = remez(101, [0, Fs/2], [1],
                     type='hilbert', fs=Fs)
 hsig = lfilter(b, 1.0, x)
+# Delay ((N - 1) / 2 = 50)
+x = np.hstack((np.zeros(50), x[:x.shape[0]-50]))
 y = - hsig * np.sin(t * wa) - np.cos(t * wa) * x
 y = y / np.max(np.abs(y)) * 32767 #???
 y = y.astype(np.int16)
